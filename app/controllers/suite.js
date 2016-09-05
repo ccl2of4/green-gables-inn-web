a707977f6db30d6b5180b0694a7774586024510a
Connor Lirot <ccl2of4@tx.rr.com>
2016-09-05 12:12:07 -0500

    add slides for all suites

--- app/controllers/suite.js
+++ app/controllers/suite.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   slideshowImages: function() {
-    let res = images['The Green Gables Suite'];
-    console.log(res);
-    return res;
-  }.property()
+    let suiteName = this.get('model.name');
+    return images[suiteName];
+  }.property('model.name')
 });
 
 let images = {
@@ -17,5 +16,27 @@ let images = {
     "assets/images/slides/the-green-gables-suite/5.jpg",
     "assets/images/slides/the-green-gables-suite/6.jpg",
     "assets/images/slides/the-green-gables-suite/7.jpg",
-    "assets/images/slides/the-green-gables-suite/8.jpg"]
+    "assets/images/slides/the-green-gables-suite/8.jpg"
+  ],
+  "The Porch Suite" : [
+    "assets/images/slides/the-porch-suite/1.jpg",
+    "assets/images/slides/the-porch-suite/2.jpg",
+    "assets/images/slides/the-porch-suite/3.jpg",
+    "assets/images/slides/the-porch-suite/4.jpg",
+    "assets/images/slides/the-porch-suite/5.jpg",
+  ],
+  "The Crows Nest" : [
+    "assets/images/slides/the-crows-nest/1.jpg",
+    "assets/images/slides/the-crows-nest/2.jpg",
+    "assets/images/slides/the-crows-nest/3.jpg",
+    "assets/images/slides/the-crows-nest/4.jpg",
+    "assets/images/slides/the-crows-nest/5.jpg",
+  ],
+  "Anne's Corner" : [
+    "assets/images/slides/annes-corner/1.jpg",
+    "assets/images/slides/annes-corner/2.jpg",
+    "assets/images/slides/annes-corner/3.jpg",
+    "assets/images/slides/annes-corner/4.jpg",
+    "assets/images/slides/annes-corner/5.jpg",
+  ]
 }
