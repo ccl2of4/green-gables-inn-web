188545d819ea5c6edcb318cabdc034907f203d02
Connor Lirot <ccl2of4@tx.rr.com>
2016-10-02 11:13:49 -0500

    bind gg-datepickers properties

--- tests/integration/components/gg-datepickers-test.js
+++ tests/integration/components/gg-datepickers-test.js
@@ -9,9 +9,28 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.set('suite', { price: 500 });
-  this.render(hbs`{{gg-datepickers suite=suite}}`);
+  this.render(hbs`{{gg-datepickers}}`);
 
   assert.equal(this.$('#checkin-datepicker').length, 1, 'Should have checkin datepicker');
   assert.equal(this.$('#checkout-datepicker').length, 1, 'Should have checkout datepicker');
+});
+
+test('binds properties', function(assert) {
+  this.set('dates', {
+    checkinDate: '8/20/2012',
+    checkoutDate: '9/17/2013'
+  });
+
+  this.render(hbs`{{gg-datepickers checkinDate=dates.checkinDate checkoutDate=dates.checkoutDate}}`);
+  
+  this.$('#checkin-datepicker')
+    .val('10/10/2014')
+    .trigger('keyup');
+
+  this.$('#checkout-datepicker')
+    .val('10/18/2014')
+    .trigger('keyup');
+
+  assert.equal(this.get('dates.checkinDate'), '10/10/2014');
+  assert.equal(this.get('dates.checkoutDate'), '10/18/2014');
 });
