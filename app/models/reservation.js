3cf30553a97eedc27a1735cc899ac704218622af
Connor Lirot <ccl2of4@tx.rr.com>
2016-11-13 15:53:57 -0600

    add fields to reservation model

--- app/models/reservation.js
+++ app/models/reservation.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
+import attr from 'ember-data/attr';
 
 export default DS.Model.extend({
-
+  suiteId: attr(),
+  clientId: attr(),
+  startDate: attr(),
+  endDate: attr(),
+  numberOfPeople: attr(),
+  comment: attr()
 });
