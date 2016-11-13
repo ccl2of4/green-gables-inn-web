e91bee71496babebf6a4364fe4a43e9db7cfcc79
Connor Lirot <ccl2of4@tx.rr.com>
2016-11-13 16:42:40 -0600

    workaround for issue with dates

--- app/controllers/booking.js
+++ app/controllers/booking.js
@@ -11,8 +11,27 @@ export default Ember.Controller.extend({
       // (it's set by the route).
       self.get('client').save().then(function() {
         self.set('reservation.clientId', self.get('client.id'));
+        self.convertToDates();
         self.get('reservation').save();
+        self.convertFromDates();
       });
     }
   },
+
+  convertToDates() {
+    this.set('reservation.startDate', new Date(this.get('reservation.startDate')));
+    this.set('reservation.endDate',   new Date(this.get('reservation.endDate')));
+  },
+
+  convertFromDates() {
+    this.set('reservation.startDate', formatDate(this.get('reservation.startDate')));
+    this.set('reservation.endDate',   formatDate(this.get('reservation.endDate')));
+  }
+
 });
+
+function formatDate(date) {
+  return (date.getMonth() + 1) +
+          "/" +  date.getDate() +
+          "/" +  date.getFullYear();
+}
